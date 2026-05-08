9836f7724d28818657155e6f83c4c9a560599a41
zmoon <zmoon92@gmail.com>
2026-05-08 16:49:56 -0500

    Fix some nc attrs
    Before:
    
    netcdf cece.emis.201012010000 {
    dimensions:
            time = UNLIMITED ; // (1 currently)
            lon = 3600 ;
            lat = 1800 ;
            lev = 1 ;
    variables:
            double time(time) ;
                    time:units = "seconds since 2010-12-01T00:00:00" ;
            double lon(lon) ;
                    lon:units = "degrees_east" ;
                    lon:long_name = "longitude" ;
                    lon:standard_name = "longitude" ;
            double lat(lat) ;
                    lat:units = "degrees_north" ;
                    lat:long_name = "latitude" ;
                    lat:standard_name = "latitude" ;
            double lev(lev) ;
                    lev:units = "1" ;
                    lev:long_name = "model_level_number\000d" ;
            double pm25(time, lev, lat, lon) ;
                    pm25:coordinates = "lon lat lev time" ;
    
    // global attributes:
                    :Conventions = "CF-1.8" ;
                    :title = "CECE Atmospheric Emissions\000t" ;
                    :institution = "CECE" ;
                    :source = "CECE - Community Emissions Computing Engin" ;
    }

--- src/io/cece_standalone_writer.cpp
+++ src/io/cece_standalone_writer.cpp
@@ -206,7 +206,7 @@ int CeceStandaloneWriter::WriteTimeStep(const std::unordered_map<std::string, Du
 
         check_nc(nc_def_var(ncid, "lev", NC_DOUBLE, 1, &dim_lev, &var_lev), "def_var lev");
         check_nc(nc_put_att_text(ncid, var_lev, "units", 1, "1"), "lev units");
-        check_nc(nc_put_att_text(ncid, var_lev, "long_name", 20, "model_level_number"), "lev long_name");
+        check_nc(nc_put_att_text(ncid, var_lev, "long_name", 18, "model level number"), "lev long_name");
 
         // Define field variables
         // If config_.fields is empty, write all fields. Else filter.
@@ -243,9 +243,9 @@ int CeceStandaloneWriter::WriteTimeStep(const std::unordered_map<std::string, Du
 
         // Add global CF convention attributes for better tool support
         check_nc(nc_put_att_text(ncid, NC_GLOBAL, "Conventions", 6, "CF-1.8"), "global Conventions");
-        check_nc(nc_put_att_text(ncid, NC_GLOBAL, "title", 28, "CECE Atmospheric Emissions"), "global title");
+        check_nc(nc_put_att_text(ncid, NC_GLOBAL, "title", 26, "CECE Atmospheric Emissions"), "global title");
         check_nc(nc_put_att_text(ncid, NC_GLOBAL, "institution", 4, "CECE"), "global institution");
-        check_nc(nc_put_att_text(ncid, NC_GLOBAL, "source", 42, "CECE - Community Emissions Computing Engine"), "global source");
+        check_nc(nc_put_att_text(ncid, NC_GLOBAL, "source", 43, "CECE - Community Emissions Computing Engine"), "global source");
 
         check_nc(nc_enddef(ncid), "enddef");
 
